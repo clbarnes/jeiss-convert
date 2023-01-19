07d0c8141be4e32fba7fdeb7aba0a45facbced1c
Chris Barnes <cbarnes@mrc-lmb.cam.ac.uk>
2023-01-19 14:16:22 +0000

    refactor tests into cli module, skip full, add metadata-only

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,9 +6,16 @@ import pytest
 
 project_dir = Path(__file__).resolve().parent.parent
 spec_dir = project_dir / "jeiss_convert" / "jeiss-specs"
-versions = [
-    int(p.stem[1:]) for p in (spec_dir / "specs").glob("*.tsv") if p.stem != "v0"
-]
+versions = [int(p.stem[1:]) for p in spec_dir.glob("specs/*.tsv") if p.stem != "v0"]
+meta_paths = {int(p.stem[1:]): p for p in spec_dir.glob("example_metadata/*.dat")}
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--skip-full",
+        action="store_true",
+        help="Skip tests which require full .dat files",
+    )
 
 
 @pytest.fixture(scope="session")
@@ -24,9 +31,25 @@ def sample_dats():
 
 @pytest.fixture(params=versions)
 def dat_path(request, sample_dats):
+    if request.config.getoption("--skip-full"):
+        pytest.skip("Requires full .dat file, --skip-large given")
+
     version = request.param
     if version not in sample_dats:
         pytest.skip(f"No sample file for version {version}")
 
     md5sum, url = sample_dats[version]
+
     return Path(pooch.retrieve(url=url, known_hash="md5:" + md5sum))
+
+
+@pytest.fixture(params=versions)
+def meta_path(request):
+    version = request.param
+
+    path = meta_paths.get(version)
+
+    if path is None:
+        pytest.skip(f"No sample metadata file for version {version}")
+
+    return path
